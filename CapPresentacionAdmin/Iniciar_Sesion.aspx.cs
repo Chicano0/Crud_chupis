b576968a96d17de7be5ea6375e6438363cd11939
MabeWasher <baenagaymer@gmail.com>
2025-03-26 11:11:55 -0600

    Optimización de index cshtml e inicio de sesion aspx

--- CapPresentacionAdmin/Iniciar_Sesion.aspx.cs
+++ CapPresentacionAdmin/Iniciar_Sesion.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.Web.Security;
+using System.Configuration;
 
 namespace CapPresentacionAdmin
 {
@@ -10,18 +11,17 @@ namespace CapPresentacionAdmin
         {
             if (IsPostBack)
             {
-                string email = Request.Form["email"];
-                string password = Request.Form["password"];
+                string email = Request.Form["email"]?.Trim();
+                string password = Request.Form["password"]?.Trim();
 
-                if (AuthenticateUser(email, password))
+                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password) && AuthenticateUser(email, password))
                 {
                     FormsAuthentication.SetAuthCookie(email, false);
                     Response.Redirect("Home");
                 }
                 else
                 {
-                    // Pasar el mensaje de error a la página
-                    loginMessage.Text = "Correo electrónico o contraseña incorrectos Intentelo De Nuevo.";
+                    loginMessage.Text = "Correo electrónico o contraseña incorrectos. Inténtelo de nuevo.";
                     loginMessage.Visible = true;
                 }
             }
@@ -30,18 +30,17 @@ namespace CapPresentacionAdmin
         private bool AuthenticateUser(string email, string password)
         {
             bool isValid = false;
-            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["crud_chupis"].ConnectionString;
+            string connectionString = ConfigurationManager.ConnectionStrings["crud_chupis"].ConnectionString;
 
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                string query = "SELECT COUNT(*) FROM Admin WHERE email = @Email AND password = @Password";
+                string query = "SELECT COUNT(*) FROM Admin WHERE email = @Email AND password = HASHBYTES('SHA2_256', @Password)";
                 using (SqlCommand cmd = new SqlCommand(query, conn))
                 {
                     cmd.Parameters.AddWithValue("@Email", email);
                     cmd.Parameters.AddWithValue("@Password", password);
                     conn.Open();
-                    int count = (int)cmd.ExecuteScalar();
-                    isValid = (count > 0);
+                    isValid = (int)cmd.ExecuteScalar() > 0;
                 }
             }
             return isValid;
